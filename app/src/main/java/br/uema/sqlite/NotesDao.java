5d11b592595169b3d97bea88a9e205fdcf17450f
Flavio Campos <flaviodcampos@hotmail.com>
2019-05-09 12:26:24 -0300

    Commit inicial

---
+++ app/src/main/java/br/uema/sqlite/NotesDao.java
@@ -0,0 +1,69 @@
+package br.uema.sqlite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotesDao {
+
+    private SQLiteDatabase database;
+    private String[] columns = {CustomSQLiteOpenHelper.COLUMN_ID,
+    CustomSQLiteOpenHelper.COLUMN_NOTES};
+    private CustomSQLiteOpenHelper sqLiteOpenHelper;
+
+    public NotesDao (Context context) {
+        sqLiteOpenHelper = new CustomSQLiteOpenHelper(context);
+    }
+
+    public void open() throws SQLException {
+        database = sqLiteOpenHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        sqLiteOpenHelper.close();
+    }
+
+    public Note create (String note) {
+        ContentValues values = new ContentValues();
+        values.put(CustomSQLiteOpenHelper.COLUMN_NOTES, note);
+        long insertId = database.insert(CustomSQLiteOpenHelper.TABLE_NOTES, null, values);
+        Cursor cursor = database.query(CustomSQLiteOpenHelper.TABLE_NOTES, columns,
+                CustomSQLiteOpenHelper.COLUMN_ID + " = " + insertId,
+                null, null, null, null);
+        cursor.moveToFirst();
+        Note newNote = new Note();
+        newNote.setId(cursor.getLong(0));
+        newNote.setNote(cursor.getString(0));
+        cursor.close();
+        return newNote;
+    }
+
+    public void delete(Note note) {
+        long id = note.getId();
+        database.delete(CustomSQLiteOpenHelper.TABLE_NOTES, CustomSQLiteOpenHelper.COLUMN_ID +
+                " = " + id, null);
+    }
+
+    public List<Note> getAll() {
+        List<Note> notes = new ArrayList<Note>();
+
+        Cursor cursor = database.query(CustomSQLiteOpenHelper.TABLE_NOTES, columns,
+                null, null, null, null, null);
+        cursor.moveToFirst();
+        while (!cursor.isAfterLast()) {
+            Note note = new Note();
+            note.setId(cursor.getLong(0));
+            note.setNote(cursor.getString(1));
+            notes.add(note);
+            cursor.moveToNext();
+        }
+        cursor.close();
+        return notes;
+    }
+
+}
